f420b38614fb6cb4f80c6521714a0989a27e71eb
cristian_spiridon <cristian.spiridon@gmail.com>
2019-04-23 16:10:15 +0100

    finished BST

--- ClosestValueInBST.playground/Contents.swift
+++ ClosestValueInBST.playground/Contents.swift
@@ -2,17 +2,17 @@ import UIKit
 
 class BST {
     
-    var value:Int
+    var value:Double
     var left:BST?
     var right:BST?
     
-    init(value:Int, leftNode:BST?, rightNode:BST?) {
+    init(value:Double, leftNode:BST?, rightNode:BST?) {
         self.value = value
         self.left  = leftNode
         self.right = rightNode
     }
     
-    func insert(value:Int) -> BST {
+    func insert(value:Double) -> BST {
         
         if value < self.value {
             if left == nil {
@@ -30,35 +30,26 @@ class BST {
         
         return self
     }
+
+    func closestHelper(target:Double, closest: Double = Double.greatestFiniteMagnitude) -> Double {
+        
+        let closeValue = abs(target - closest) > abs(target - value) ? value : closest
     
-    func printme() {
-        print(value)
-        
-        if left != nil {
-            self.left?.printme()
+        if let leftNode = left, value > target {
+            return leftNode.closestHelper(target: target, closest: closeValue)
         }
-        
-        print("end_left")
-        
-        if right != nil {
-            self.right?.printme()
+    
+        if let rightNode = right, value < target {
+            return rightNode.closestHelper(target: target, closest: closeValue)
         }
-        print("end_right")
-        
+
+        return closeValue
     }
-    
+
 }
 
 var bstTree = BST(value: 10, leftNode: nil, rightNode: nil)
-
 bstTree.insert(value: 5).insert(value: 15).insert(value: 2).insert(value: 5).insert(value: 13).insert(value: 22).insert(value: 1).insert(value: 14)
 
+print(bstTree.closestHelper(target: 23))
 
-
-
-func findClosestValue(bstTree:BST, targetValue:Int) -> Int? {
-    let closestValue = Double.greatestFiniteMagnitude
-    
-    
-    return nil
-}
